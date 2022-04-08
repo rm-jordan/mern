cb77f249e98065258f560895f06683b0e364c1ff
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-04-08 10:09:54 -0400

    created routes folder, cleaning up folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("SERVER WORKING MATE!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 6000;
+
+const app = express();
+
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({ message: "Get the goals!" });
+});
+
+app.listen(port, () => console.log(`server started on ${port}`));
