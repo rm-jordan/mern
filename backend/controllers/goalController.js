1a9e98cb54d81193219c338275a0812b19a6825b
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-04-09 08:34:36 -0400

    route setup prior to setup of error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,34 @@
 // @desc Get Goals
 // @route GET /api/goals
-// @access Private
+// @access Private - post authentication
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals!" });
 };
 
-module.exports = {
-  getGoals,
+// @desc Set Goal
+// @route POST /api/goals
+// @access Private - post authentication
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Create goals!" });
+};
+
+// @desc Update Goal
+// @route PUT /api/goals/:id
+// @access Private - post authentication
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+
+  // @desc Delete Goal
+  // @route DELETE /api/goals/:id
+  // @access Private - post authentication
+  const deleteGoal = (req, res) => {
+    res.status(200).json({ message: `This deletes ${req.params.id}` });
+  };
+
+  module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+  };
 };
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,20 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getGoals } = require("../controllers/goalController");
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.get("/", getGoals);
+router
+  .route("/")
+  .get(getGoals)
+  .post(setGoal);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Create goals!" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `This deletes ${req.params.id}` });
-});
+router
+  .route("/:id")
+  .put(updateGoal)
+  .delete(deleteGoal);
 
 module.exports = router;
