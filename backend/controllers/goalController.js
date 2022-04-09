90aefee93154f3d0d60ad533df1e104f6b28f0f7
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-04-09 09:40:23 -0400

    added middleware, testing in postman done

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,41 @@
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private - post authentication
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
   res.status(200).json({ message: "Get goals!" });
 };
 
 // @desc Set Goal
 // @route POST /api/goals
 // @access Private - post authentication
-const setGoal = (req, res) => {
+
+// mongoose operates with a promise therefore async/await
+
+const setGoal = async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error(" Please add a text field!");
+  }
   res.status(200).json({ message: "Create goals!" });
 };
 
 // @desc Update Goal
 // @route PUT /api/goals/:id
 // @access Private - post authentication
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
 
-  // @desc Delete Goal
-  // @route DELETE /api/goals/:id
-  // @access Private - post authentication
-  const deleteGoal = (req, res) => {
-    res.status(200).json({ message: `This deletes ${req.params.id}` });
-  };
+// @desc Delete Goal
+// @route DELETE /api/goals/:id
+// @access Private - post authentication
+const deleteGoal = async (req, res) => {
+  res.status(200).json({ message: `This deletes ${req.params.id}` });
+};
 
-  module.exports = {
-    getGoals,
-    setGoal,
-    updateGoal,
-    deleteGoal,
-  };
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
 };
