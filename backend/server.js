bd0d7c41854cbcb4e2696ce4bee7839b031d7558
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-04-12 07:54:32 -0400

    connected to DB, underline in console, full connection to cluster

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 
 const { errorHandler } = require("./middleware/errorMiddleware");
 
 const port = process.env.PORT || 6000;
+
+connectDB();
 
 const app = express();
 
