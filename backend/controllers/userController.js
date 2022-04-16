2a45c4b1cf9c4836a91853092a1a1fdc44d5c308
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-04-16 09:07:05 -0400

    set up login functionality, check with postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,42 @@ const User = require("../models/userModel");
 // @route POST api/users
 // @access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("please add all fields");
+  }
+
+  // Check to see if the user does exist
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists!");
+  }
+
+  //hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create a user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Authenticate a user
