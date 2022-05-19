c09b700491954305e7d321aa687fe12c66c915fe
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-05-19 08:45:19 -0400

    created authslice for tokens, updated dependencies

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,12 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// get user from local storage
+const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
