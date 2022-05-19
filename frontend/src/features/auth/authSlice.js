c39d3707415c06dc5732710fef1777d790f1523b
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-05-19 09:22:13 -0400

    redux setup for registration completed, need to hook up register form

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -45,7 +45,24 @@ export const authSlice = createSlice({
     },
   },
 
-  extraReducers: () => {},
+  //check for pending, fulfilled and rejected
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
 });
 
 export const { reset } = authSlice.actions;
