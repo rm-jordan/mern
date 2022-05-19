90b2d2c4ca96b5cd33a427f2ea0d029b27ea7f1d
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-05-19 09:05:54 -0400

    started redux for user registration

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 // get user from local storage
 const user = JSON.parse(localStorage.getItem("user"));
@@ -10,3 +11,42 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+// Register user
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// after everything is set, reset values back to false
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = "";
+    },
+  },
+
+  extraReducers: () => {},
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
